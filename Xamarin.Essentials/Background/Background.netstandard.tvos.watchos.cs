914460aefe79492b0e1f337ab2ab3edfbecbf6fb
Afshin Arani <afshinarani79@gmail.com>
2022-07-27 00:33:25 +0800

    Background tasks support
    Fixes https://github.com/xamarin/Essentials/issues/409
    
    Upstream PR: https://github.com/xamarin/Essentials/pull/1674
    
    (Removed .netstandard1.0 target framework because it doesn't support
    OS checks.)
    
    Co-authored-by: Taras <mail@webwarrior.ws>

---
+++ Xamarin.Essentials/Background/Background.netstandard.tvos.watchos.cs
@@ -0,0 +1,15 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Xamarin.Essentials.Background
+{
+    public static partial class Background
+    {
+        internal static void PlatformStart()
+        {
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+                Background.StartJobs();
+            else
+                throw ExceptionUtils.NotSupportedOrImplementedException;
+        }
+    }
+}
