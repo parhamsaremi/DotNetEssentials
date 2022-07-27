5586709995bac95a1163428c4f4c31d26820ee5e
Andres G. Aragoneses <knocte@gmail.com>
2022-07-27 18:51:15 +0800

    Clipboard: add GTK support
    Forward-ported from https://github.com/xamarin/Essentials/pull/1290
    
    Which was based on https://github.com/stavroskasidis/XamarinClipboardPlugin/commit/b213a88d3d9d85d3600cd5084a53c670f51f0fb7
    (whose PR was https://github.com/stavroskasidis/XamarinClipboardPlugin/pull/7 )
    
    Co-authored-by: Taras <reg@webwarrior.ws>

---
+++ Xamarin.Essentials/Clipboard/Clipboard.netstandard.cs
@@ -0,0 +1,66 @@
+﻿using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+
+namespace Xamarin.Essentials
+{
+    public static partial class Clipboard
+    {
+        static Task PlatformSetTextAsync(string text)
+        {
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+                return GTKSetTextAsync(text);
+            else
+                throw ExceptionUtils.NotSupportedOrImplementedException;
+        }
+
+        static Task GTKSetTextAsync(string text)
+        {
+            var clipboardAtom = Gdk.Atom.Intern("CLIPBOARD", false);
+            var clipboard = Gtk.Clipboard.Get(clipboardAtom);
+            clipboard.Text = text;
+            return Task.FromResult(0);
+        }
+
+        static bool PlatformHasText
+        {
+            get
+            {
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+                    return GTKHasText;
+                else
+                    throw ExceptionUtils.NotSupportedOrImplementedException;
+            }
+        }
+
+        static bool GTKHasText
+        {
+            get
+            {
+                var clipboardAtom = Gdk.Atom.Intern("CLIPBOARD", false);
+                var clipboard = Gtk.Clipboard.Get(clipboardAtom);
+                return clipboard.WaitIsTextAvailable();
+            }
+        }
+
+        static Task<string> PlatformGetTextAsync()
+        {
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+                return GTKetTextAsync();
+            else
+                throw ExceptionUtils.NotSupportedOrImplementedException;
+        }
+
+        static Task<string> GTKetTextAsync()
+        {
+            var clipboardAtom = Gdk.Atom.Intern("CLIPBOARD", false);
+            var clipboard = Gtk.Clipboard.Get(clipboardAtom);
+            return Task.FromResult(clipboard.WaitForText());
+        }
+
+        static void StartClipboardListeners()
+            => throw ExceptionUtils.NotSupportedOrImplementedException;
+
+        static void StopClipboardListeners()
+            => throw ExceptionUtils.NotSupportedOrImplementedException;
+    }
+}
--- Xamarin.Essentials/MainThread/MainThread.netstandard.cs
+++ Xamarin.Essentials/MainThread/MainThread.netstandard.cs
@@ -4,10 +4,34 @@ namespace Xamarin.Essentials
 {
     public static partial class MainThread
     {
+#if GTK
+#pragma warning disable SA1130
+        static void PlatformBeginInvokeOnMainThread(Action action) => Gtk.Application.Invoke(delegate { action(); });
+
+        static int? uiThreadId = null;
+
+        static bool PlatformIsMainThread
+        {
+            get
+            {
+                if (uiThreadId == null)
+                {
+                    Gtk.Application.Invoke(delegate { uiThreadId = Thread.CurrentThread.ManagedThreadId; });
+                    return false;
+                }
+                else
+                {
+                    return System.Threading.Thread.CurrentThread.ManagedThreadId == uiThreadId;
+                }
+            }
+        }
+#pragma warning restore SA1130
+#else
         static void PlatformBeginInvokeOnMainThread(Action action) =>
             throw ExceptionUtils.NotSupportedOrImplementedException;
 
         static bool PlatformIsMainThread =>
             throw ExceptionUtils.NotSupportedOrImplementedException;
+#endif
     }
 }
