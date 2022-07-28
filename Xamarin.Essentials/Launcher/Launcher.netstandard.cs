7f6e4668a00d71ea4cffe06cf496bc5554b52861
webwarrior <reg@webwarrior.ws>
2022-07-28 12:03:17 +0200

    Launcher: implemented opening of URI on Linux/GTK
    Needed to fix https://gitlab.com/nblockchain/geewallet/-/issues/177

---
+++ Xamarin.Essentials/Launcher/Launcher.netstandard.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+
+namespace Xamarin.Essentials
+{
+    public static partial class Launcher
+    {
+        static Task<bool> PlatformCanOpenAsync(Uri uri)
+        {
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+                return Task.FromResult(true);
+            else
+                throw ExceptionUtils.NotSupportedOrImplementedException;
+        }
+
+        static async Task PlatformOpenAsync(Uri uri)
+        {
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+                await GTKTryOpenAsync(uri);
+            else
+                throw ExceptionUtils.NotSupportedOrImplementedException;
+        }
+
+        static Task PlatformOpenAsync(OpenFileRequest request) =>
+            throw ExceptionUtils.NotSupportedOrImplementedException;
+
+        static async Task<bool> PlatformTryOpenAsync(Uri uri)
+        {
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+                return await GTKTryOpenAsync(uri);
+            else
+                throw ExceptionUtils.NotSupportedOrImplementedException;
+        }
+
+        static async Task<bool> GTKTryOpenAsync(Uri uri)
+        {
+            string stdout, stderr;
+            int exitCode;
+            var task = Task.Run(
+                () => GLib.Process.SpawnCommandLineSync("xdg-open " + uri.ToString(), out stdout, out stderr, out exitCode));
+            var result = await task;
+            return result;
+        }
+    }
+}
