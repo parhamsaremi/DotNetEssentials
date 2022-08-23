c53fc37f7b4e9a26469dc472dc3b6c3da921b20d
Parham Saremi <parhaamsaremi@gmail.com>
2022-08-23 13:36:35 +0430

    WIP
    add runtime check to MainThread.netstandard.cs

--- Xamarin.Essentials/MainThread/MainThread.netstandard.cs
+++ Xamarin.Essentials/MainThread/MainThread.netstandard.cs
@@ -4,34 +4,37 @@ namespace Xamarin.Essentials
 {
     public static partial class MainThread
     {
-#if GTK
 #pragma warning disable SA1130
-        static void PlatformBeginInvokeOnMainThread(Action action) => Gtk.Application.Invoke(delegate { action(); });
-
+        static void PlatformBeginInvokeOnMainThread(Action action){
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+                Gtk.Application.Invoke(delegate { action(); });
+            else
+                throw ExceptionUtils.NotSupportedOrImplementedException;
+        }
         static int? uiThreadId = null;
 
         static bool PlatformIsMainThread
         {
             get
             {
-                if (uiThreadId == null)
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
                 {
-                    Gtk.Application.Invoke(delegate { uiThreadId = Thread.CurrentThread.ManagedThreadId; });
-                    return false;
+                    if (uiThreadId == null)
+                    {
+                        Gtk.Application.Invoke(delegate { uiThreadId = Thread.CurrentThread.ManagedThreadId; });
+                        return false;
+                    }
+                    else
+                    {
+                        return System.Threading.Thread.CurrentThread.ManagedThreadId == uiThreadId;
+                    }
                 }
                 else
                 {
-                    return System.Threading.Thread.CurrentThread.ManagedThreadId == uiThreadId;
+                    throw ExceptionUtils.NotSupportedOrImplementedException;
                 }
             }
         }
 #pragma warning restore SA1130
-#else
-        static void PlatformBeginInvokeOnMainThread(Action action) =>
-            throw ExceptionUtils.NotSupportedOrImplementedException;
-
-        static bool PlatformIsMainThread =>
-            throw ExceptionUtils.NotSupportedOrImplementedException;
-#endif
     }
 }
